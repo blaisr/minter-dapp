827dcd777252e7710e00e8301ea8f2d8d897b061
Remi Blais <spotmb13i@Remis-MacBook-Pro.local>
2022-04-09 18:40:24 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xD27c2089402756e1a26184389826DBf95178D6bF"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
